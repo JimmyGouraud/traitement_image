6e5d44bce1786bc432a259b9315b2f4152eab56e
Krazyxx <jimmy.gouraud@hotmail.fr>
2017-02-12 22:36:49 +0100

    fixed error filter

--- zoom/filter.c
+++ zoom/filter.c
@@ -22,20 +22,20 @@ double WF(char* filter_name)
     return 0.5;
   }
   if (strcmp(filter_name, "tent") == 0) {
-    return 2;
+    return 1.0;
   }
   if (strcmp(filter_name, "bell") == 0) {
     return 1.5;
   }
   if (strcmp(filter_name, "mitch") == 0) {
-    return 4;
+    return 2.0;
   }
   
   fprintf(stderr, "Wrong <filter-name> : filter-name={box, tent, bell, mitch}.\n");
   exit(EXIT_FAILURE);
 }
 
-double h(char* filter_name, int x)
+double h(char* filter_name, double x)
 {
   if (strcmp(filter_name, "box") == 0) {
     return box(x);
@@ -58,15 +58,15 @@ double h(char* filter_name, int x)
 double box(double x)
 {
   if (-0.5 <= x && x <= 0.5) {
-    return 1;
+    return 1.0;
   }
-  return 0;
+  return 0.0;
 }
 
 double tent(double x)
 {
   if (-1 <= x && x <= 1){
-    return 1 - abs(x);
+    return 1 - fabs(x);
   }
 
   return 0;
@@ -79,8 +79,8 @@ double bell(double x)
     return -(x*x) + 0.75;
   }
 
-  if (0.5 < absX && absX <= 0.75){
-    return 0.5 * (absX - 0.75) * (absX - 1.5);
+  if (absX <= 1.5){
+    return 0.5 * (absX - 1.5) * (absX - 1.5);
   }
 
   return 0;
@@ -91,37 +91,28 @@ double mitchellNetravali(double x)
   double absX = fabs(x);
   if (-2 <= x && x <= 2) {
     if (-1 <= x && x <= 1) {
-      return (7/6) * absX * absX * absX - 2 * x * x + 8/9;
+      return (7/6.0) * absX * absX * absX - 2 * x * x + 8/9.0;
     }
-    return(-7/18) * absX * absX * absX + 2 * x * x - (10/3) * absX + 16/9;
+    return (-7/18.0) * absX * absX * absX + 2 * x * x - (10/3.0) * absX + 16/9.0;
   }
   
   return 0;
 }
 
 
-pnm rotation (int imd_rows, int imd_cols, int rows, pnm ims) {
-  pnm imd = pnm_new(imd_cols, imd_rows, PnmRawPpm);
-  
-  for (int i = 0; i < imd_cols; ++i) {
-    for (int j = 0; j < imd_rows; ++j) {
-      for (int k = 0; k < 3; ++k){
-	unsigned short value = 0;
-	if (j < rows){
-	  value = pnm_get_component(ims, j, i, 0);
-	}
-	pnm_set_component(imd, i, j, k, value);
+void rotation (int rows, int cols, pnm ims, pnm imd) {  
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      for (int k = 0; k < 3; ++k) {
+	pnm_set_component(imd, j, i, k, pnm_get_component(ims, i, j, k));
       }
     }
   }
-  
-  return imd;
 }
 
 
-pnm interpolation(char* filter_name, int factor, int imd_rows, int imd_cols, int cols, pnm ims)
+void interpolation(char* filter_name, int factor, int imd_rows, int imd_cols, int cols, pnm ims, pnm imd)
 {
-  pnm imd = pnm_new(imd_cols, imd_rows, PnmRawPpm);
   for (int j_prime = 0; j_prime < imd_cols; ++j_prime){
     for (int i = 0; i < imd_rows; ++i) {
       double j = j_prime / (double)factor;
@@ -140,8 +131,6 @@ pnm interpolation(char* filter_name, int factor, int imd_rows, int imd_cols, int
       }
     }
   }
-  
-  return imd;
 }
 
 
@@ -153,16 +142,20 @@ void filter(int factor, char* filter_name, char* ims_name, char* imd_name)
   int imd_rows = rows * factor;
   int imd_cols = cols * factor;
 
-  pnm imd_dim1 = interpolation(filter_name, factor, rows, imd_cols, cols, ims);
-  pnm imd_rotation = rotation(imd_rows, imd_cols, rows, imd_dim1);
-  pnm imd = interpolation(filter_name, factor, imd_rows, imd_cols, cols, imd_rotation);
-  
+  pnm imd_dim1 = pnm_new(imd_cols, rows, PnmRawPpm);
+  interpolation(filter_name, factor, rows, imd_cols, cols, ims, imd_dim1);
+
+  pnm imd_rotation = pnm_new(cols, imd_rows, PnmRawPpm);
+  rotation(rows, imd_cols, imd_dim1, imd_rotation);
+
+  pnm imd_dim2 = pnm_new(imd_cols, imd_rows, PnmRawPpm);
+  interpolation(filter_name, factor, imd_rows, imd_cols, cols, imd_rotation, imd_dim2);
+
+  pnm imd = pnm_new(imd_cols, imd_rows, PnmRawPpm);
+  rotation(imd_rows, imd_cols, imd_dim2, imd);
+
   pnm_save(imd, PnmRawPpm, imd_name);
 
-  (void) factor;
-  (void) filter_name;
-  (void) ims_name;
-  (void) imd_name;
 }
 
 
